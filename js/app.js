22c9a705a748e6be61b71f5ac0de8615b356f872
Diego de la Cruz Retuerto <U25319701@utp.edu.pe>
2025-11-15 23:24:09 -0500

    update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
+const birdImg = new Image();
+birdImg.src = "img/bird+.png";
+
+const pipeImg = new Image();
+pipeImg.src = "img/block.png";
+
+const bgImg = new Image();
+bgImg.src = "img/fondo.avif";
+
 let birdY = 150;
 let birdVelocity = 0;
 const gravity = 0.6;
@@ -22,11 +31,26 @@ function drawBird() {
   ctx.fill();
 }
 
+/*
 function drawPipe(pipe) {
   ctx.fillStyle = "green";
   ctx.fillRect(pipe.x, 0, 50, pipe.top);
   ctx.fillRect(pipe.x, pipe.top + pipe.gap, 50, canvas.height - pipe.top - pipe.gap);
 }
+*/
+
+function drawPipe(pipe) {
+  // Tubo superior (invertido)
+  ctx.save();
+  ctx.translate(pipe.x + 25, pipe.top); // centro del tubo
+  ctx.scale(1, -1); // voltea verticalmente
+  ctx.drawImage(pipeImg, -25, 0, 50, canvas.height); // dibuja desde el centro
+  ctx.restore();
+
+  // Tubo inferior
+  ctx.drawImage(pipeImg, pipe.x, pipe.top + pipe.gap, 50, canvas.height);
+}
+
 
 function updatePipes() {
   if (frame % 90 === 0) {
@@ -64,6 +88,10 @@ function drawScore() {
 
 function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Dibuja fondo
+  ctx.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
+
   birdVelocity += gravity;
   birdY += birdVelocity;
 
